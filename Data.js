12b4234f6815abdee4d8c91d82c19e2f63273dbf
areeba <108215981+areeba230@users.noreply.github.com>
2023-05-27 18:19:52 +0500

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,23 @@
+
+
+    const question = "What is your favorite programming language?"
+  
+    const choices = [
+  
+        { "id": 1, "label": "JavaScript", "votes": 0 },
+    
+        { "id": 2, "label": "Python", "votes": 0 },
+    
+        { "id": 3, "label": "Java", "votes": 0 },
+    
+        { "id": 4, "label": "C#", "votes": 0 }
+    
+    
+      ]
+    
+
+
+
+export default choices
+export { question}
+
